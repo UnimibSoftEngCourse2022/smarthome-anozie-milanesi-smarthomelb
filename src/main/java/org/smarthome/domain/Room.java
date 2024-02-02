0049781340a52ac47e14598d88554634f7377bf4
Luca Milanesi <l.milanesi7@campus.unimib.it>
2024-02-02 18:01:04 +0100

    estesa la classe Room.java a RoomSimulation.java

--- src/main/java/org/smarthome/domain/Room.java
+++ src/main/java/org/smarthome/domain/Room.java
@@ -1,15 +1,17 @@
 package org.smarthome.domain;
 
 import org.smarthome.domain.illumination.Light;
+import org.smarthome.domain.simulation.RoomSimulation;
 
 import java.util.List;
 
-public class Room {
+public class Room extends RoomSimulation {
 
     private final String name;
     private final List<Light> lights;
 
     public Room(String name, List<Light> lights) {
+        super();
         this.name = name;
         this.lights = lights;
     }
