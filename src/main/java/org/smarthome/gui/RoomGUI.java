9d6ad02d19aab83e7131c4b133c1d46b6ad2b392
Bryan Anozie <b.anozie@campus.unimib.it>
2024-02-02 18:27:39 +0100

    Modifica SmartHomeGUI e RoomGUI aggiunto file .idea in .gitignore

--- src/main/java/org/smarthome/gui/RoomGUI.java
+++ src/main/java/org/smarthome/gui/RoomGUI.java
@@ -1,6 +1,8 @@
 package org.smarthome.gui;
 
 import org.smarthome.domain.Room;
+import org.smarthome.domain.illumination.Light;
+import org.smarthome.domain.illumination.LightOn;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,29 +10,34 @@ import java.awt.*;
 public class RoomGUI extends JFrame {
     private JPanel panel1;
     private JButton lightControlButton;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private Room room;
+    private JScrollBar scrollBar1;
 
-    public RoomGUI() throws HeadlessException {
+    public RoomGUI(Room room) throws HeadlessException {
 
+        setTitle(room.getName());
+        setLayout(new FlowLayout());
         setContentPane(panel1);
         setSize(400,400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+        for(Light l : room.getIllumination().getLights()) {
+            add(createLightButton(l));
+        }
 
-        lightControlButton.addActionListener(e -> {
+    }
 
-            radioButton1.doClick();
-            radioButton2.doClick();
-            radioButton3.doClick();
+    public JRadioButton createLightButton(Light l) {
 
+        JRadioButton lightButton = new JRadioButton("Light");
+        lightButton.setVisible(true);
+
+        lightButton.addActionListener(e -> {
+            l.setLightState(new LightOn(l));
+            lightButton.setEnabled(true);
         });
+
+        return lightButton;
     }
 
-    public static void main(String[] args) {
-        new RoomGUI();
-    }
 }
--- src/main/java/org/smarthome/gui/SmartHomeGUI.java
+++ src/main/java/org/smarthome/gui/SmartHomeGUI.java
@@ -1,35 +1,43 @@
 package org.smarthome.gui;
 
+import org.smarthome.domain.Room;
 import org.smarthome.domain.SmartHome;
+import org.smarthome.domain.illumination.Light;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SmartHomeGUI extends JFrame {
     private JPanel panel1;
-    private JButton room1;
-    private JButton room2;
-    private SmartHome home;
+    private JScrollBar scrollBar1;
 
-    public SmartHomeGUI() {
+    public SmartHomeGUI(SmartHome home) {
 
         setContentPane(panel1);
+        setLayout(new FlowLayout());
         setSize(400, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
-        room1.addActionListener(e ->  {
-            new RoomGUI().setVisible(true);
-        });
-
-        room2.addActionListener(e ->  {
-            new RoomGUI().setVisible(true);
-        });
+        for(Room r : home.getRooms()) {
+            add(createRoomButton(r));
+        }
 
     }
 
-    public static void main(String[] args) {
-        new SmartHomeGUI();
+    public JButton createRoomButton(Room r) {
 
+        JButton roomButton = new JButton("Room");
+        roomButton.setVisible(true);
+
+        roomButton.addActionListener(e -> {
+            new RoomGUI(r);
+        });
+
+        return roomButton;
     }
+
 }
